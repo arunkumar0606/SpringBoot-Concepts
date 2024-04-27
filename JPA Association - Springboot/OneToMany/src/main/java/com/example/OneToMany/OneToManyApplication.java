c43890c2a7fd70bef6cb442ef46255ab9fbe3f16
Arun Kumar D <74062427+arunkumar0606@users.noreply.github.com>
2024-04-27 11:54:07 +0530

    Added OneToMany Folder

---
+++ JPA Association - Springboot/OneToMany/src/main/java/com/example/OneToMany/OneToManyApplication.java
@@ -0,0 +1,13 @@
+package com.example.OneToMany;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneToManyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OneToManyApplication.class, args);
+	}
+
+}
